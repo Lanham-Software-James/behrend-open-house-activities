ae762be01967e16716e9cbea00eaf2af55b4c7cc
Lanham-Software-James <james.r.lanham2021@gmail.com>
2026-09-22 08:51:10 -0400

    Implementing the sitewide header and footer according to Penn State brand guide.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterLink, RouterOutlet],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('behrend-open-house-activities');
+  protected readonly year = new Date().getFullYear();
 }
