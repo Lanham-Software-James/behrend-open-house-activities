796449787a581b41753de2a7b96c0d650a8a4eea
Lanham-Software-James <james.r.lanham2021@gmail.com>
2026-09-22 09:23:44 -0400

    Standing up my three activity components to build the homepage around.

---
+++ src/app/activities/binary-challenge/binary-challenge.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-binary-challenge',
+  templateUrl: './binary-challenge.html',
+  styleUrl: './binary-challenge.css',
+})
+export class BinaryChallenge {}
